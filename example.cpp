85f1981b0d91f1e04fb96008bd08fc114465581d
U-BSD\418331 <418331@SHS-BYBF6X3.beaverton.k12.or.us>
2023-09-11 11:18:19 -0700

    first commit

---
+++ example.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int secretNum = 0;
+  int input = 0;
+  bool stillPlaying = true;
+  char playAgain;
+  bool play = true;
+  
+  srand (time(NULL));
+  secretNum = (rand() % 100) + 1;
+  
+  while(stillPlaying == true){
+    cout << "What is your guess?" << endl;
+    cin >> input;
+
+    if(input == secretNum){
+      play = true;
+      cout << "Good Job!" << endl;
+	cout << "Play Again? Please enter y or n." << endl; 
+      while(play == true){
+	cin >> playAgain;
+	if(playAgain == 'n'){
+	  stillPlaying = false;
+	  cout << "Bye!";
+	  play = false;
+	}
+	else if(playAgain == 'y'){
+	  stillPlaying = true;
+	  srand (time(NULL));
+	  secretNum = (rand() % 100) + 1;
+	  play = false;
+	}
+	else{
+	  cout << "Please enter y or n." << endl;
+	  play = true;
+	}
+      }
+    }
+    else if(input < secretNum){
+      cout << "Too Low!" << endl;
+    }
+    else{
+      cout << "Too High!" << endl;
+    }
+  }
+  return 0;
+}
